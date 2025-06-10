25bc4a5912523a996d5c3fcf25789b69bf910694
Simran Singh <simransi1911@gmail.com>
2025-06-10 23:33:23 +0530

    Add files via upload
    Initial uploads of the House Price Prediction Project.

---
+++ main.py
@@ -0,0 +1,39 @@
+#Importing the libraries
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+#importing the dataset
+dataset = pd.read_csv('BostonHousing.csv', delimiter=',')
+
+#Features and Target
+X = dataset.drop('medv', axis=1)
+y = dataset['medv']
+
+#splitting the data into train and test sets via the 80-20 rule
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#training the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+#prediction
+y_pred = model.predict(X_test)
+
+#evaluation
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Mean Squared Error:",mse)
+print("r2 score:", r2)
+
+#visualize
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+plt.title("Actual vs Predicted House Prices")
+plt.show()
